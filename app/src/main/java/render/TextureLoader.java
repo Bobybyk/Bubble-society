0e699689e3eabd95b20069b030b4963653975e12
SirHenryAllen <matt.lefranc@gmail.com>
2023-08-23 22:46:46 +0200

    texture loader and tileSheet jdoc

--- app/src/main/java/render/TextureLoader.java
+++ app/src/main/java/render/TextureLoader.java
@@ -3,6 +3,11 @@ package render;
 public class TextureLoader {
     private Texture[] frames;
 
+    /**
+     * @brief create a texture loader
+     * @param amount amount of textures
+     * @param filename path to the textures
+     */
     public TextureLoader(int amount, String filename) {
         this.frames = new Texture[amount];
         for (int i = 0; i < amount; i++) {
@@ -10,6 +15,10 @@ public class TextureLoader {
         }
     }
 
+    /**
+     * @brief get the textures
+     * @return textures as an array of textures
+     */
     public Texture[] getTextures() {
         return frames;
     }
